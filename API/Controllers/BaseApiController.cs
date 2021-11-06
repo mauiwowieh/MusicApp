8b1e8147f9526a4a268f6389ab829440e3cda474
mauiwowieh <66336545+mauiwowieh@users.noreply.github.com>
2021-11-06 13:27:28 +0100

    fourth

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public BaseApiController(DataContext context)
+        {
+            _context = context;
+        }
+    }
+    
+}
